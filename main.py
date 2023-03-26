6524fae9f4c44fe708fef52ee3b137f8cb3eba28
isabellakieres <127750454+isabellakieres@users.noreply.github.com>
2023-03-26 23:02:56 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,97 +1,40 @@
-def name_age():
-    print("Type in your name")
-    x = input()
-    print("Type in your age")
-    y = input()
+def count_integer(numbers, integer):
+    count = 0
 
-    print("Hello " + x + ". Your age is: " + y)
+    for i in range(len(numbers)):
+        if numbers[i] == integer:
+            count += 1
 
+    if count >= 1:
+        return count
 
-    return
-
-def name_age_2():
-    print("Type in your name")
-    x = input()
-    print("Type in your age")
-    y = input()
-
-    print(x+y)
-    return
+    elif count < 1:
+        return "42"
 
 
-def swap_integers(num1, num2):
-    x = num1
-    y = num2
-
-    print(f'X={x}')
-    print(f'Y={y}')
-
-    x = num2
-    y = num1
-
-    print(f'X={x}')
-    print(f'Y={y}')
-
-    print(f'{x}'+f'{y}')
-    return
-
-def check_numbers(num1, num2):
-    if (num1 % 6 == 0 or num2 % 6 == 0):
-        if (num1 % 10 == 0 and num2 % 10 == 0):
-            return True
-        else:
-            return False
-
-    return False
+def list_func(numbers, integer):
+# I don't know how to solve this one
 
 
-def sum_up(num1, num2):
-
-    if (num1 > num2):
-
-        return 0
-
-    if (num1 < 0 or num2 < 0):
-
-        return 0
-
-    y = 0
-    for x in range (num1, num2+1):
-        y = y + x
-
-    return y
+def compare_lists(list1, list2):
+    common_elements = []
+    for element in list1:
+        if element in list2:
+            common_elements.append(element)
+    return common_elements
 
 
-def circle_area(r1, r2, r3):
-
-  x = r1*r1*3.141
-  y = r2*r2*3.141
-  z = r3*r3*3.141
-
-  return x + y +z
+def remove_duplicates(lst):
+    return list(set(lst))
 
 
-def check_string(text):
-    if text.startswith("a") or text.startswith("A") or text.endswith("a"):
-        return True
-    else:
-        return False
-
-def triangle(rows):
-    for i in range(rows):
-        x = "*"
-        for q in range(i):
-            x = x + "*"
-        print(x)
-
-
-
-
-
-
-
-
-
+def dict_func(dictionary):
+    type = dictionary.get("Type", "unknown type")
+    brand = dictionary.get("Brand", "unknown brand")
+    price = dictionary.get("Price", "unknown price")
+    print("You have a {} from {} that costs {}.".format(type, brand, price))
+    dictionary["OS"] = "Linux"
+    print(dictionary)
 
 
 
